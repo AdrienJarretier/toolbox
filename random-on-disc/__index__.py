c8d1960cbb86be0fdce0434e9d97e6c9d9b1b554
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-09-22 15:24:58 +0200

    pick points distributed over the surface of a disk and plot them

---
+++ random-on-disc/__index__.py
@@ -0,0 +1,27 @@
+import random
+import math
+
+from Point import Point
+from plot import *
+
+RADIUS = 10
+
+points = []
+
+
+def pickPoint():
+
+    pickedAngle = random.uniform(0, 2*math.pi)
+    pickedRadius = random.uniform(0, RADIUS**2)
+
+    x = math.sqrt(pickedRadius)*math.cos(pickedAngle)
+    y = math.sqrt(pickedRadius)*math.sin(pickedAngle)
+
+    return Point(x, y)
+
+
+for i in range(2000):
+
+    points.append(pickPoint())
+
+plotPoints(points, [[0, 0, 0, 1/4]])
--- random-on-disc/plot.py
+++ random-on-disc/plot.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 from Point import Point
 
 
-def plotPoints(points: list[Point]):
+def plotPoints(points: list[Point], color=[[0, 0, 0, 1/5]]):
     transposedPoints = list(map(list, zip(*points)))
 
-    plt.scatter(transposedPoints[0], transposedPoints[1], c=[[0, 0, 0, 1/5]])
+    plt.figure(1, [7.2,7.2])
+    plt.scatter(transposedPoints[0], transposedPoints[1], c=color)
     plt.show()
