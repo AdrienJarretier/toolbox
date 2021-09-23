40df73347084be7147b0349a787446dec25bcafd
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-09-23 04:29:10 +0200

    load and display a background image

--- random-on-disc/__index__.py
+++ random-on-disc/__index__.py
@@ -1,10 +1,14 @@
 import random
 import math
+import matplotlib.image as mpimg
 
 from Point import Point
 from plot import *
 
-RADIUS = 10
+
+img = mpimg.imread('test_image.jpg')
+
+RADIUS = max(img.shape[0], img.shape[1])/2
 
 points = []
 
@@ -14,14 +18,14 @@ def pickPoint():
     pickedAngle = random.uniform(0, 2*math.pi)
     pickedRadius = random.uniform(0, RADIUS**2)
 
-    x = math.sqrt(pickedRadius)*math.cos(pickedAngle)
-    y = math.sqrt(pickedRadius)*math.sin(pickedAngle)
+    x = math.sqrt(pickedRadius)*math.cos(pickedAngle)+RADIUS
+    y = math.sqrt(pickedRadius)*math.sin(pickedAngle)+RADIUS
 
     return Point(x, y)
 
 
-for i in range(2000):
+for i in range(10):
 
     points.append(pickPoint())
 
-plotPoints(points, RADIUS, color=[[0, 0, 0, 1/4]])
+plotPoints(points, RADIUS, color=[[1, 0, 0, 1/2]], backgroundImage=img)
--- random-on-disc/plot.py
+++ random-on-disc/plot.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 from Point import Point
 
 
-def plotPoints(points: list[Point], radius=1.0, color=[[0, 0, 0, 1/5]]):
+def plotPoints(points: list[Point], radius=1.0, color=[[0, 0, 0, 1/5]], backgroundImage=None):
+
     transposedPoints = list(map(list, zip(*points)))
 
     fig = plt.figure(1, [7.2, 7.2])
-    circle = plt.Circle((0, 0), radius, fill=False)
+
+    if backgroundImage is not None:
+        plt.imshow(backgroundImage)
+
+    circle = plt.Circle((radius, radius), radius, fill=False, color='red')
     ax = fig.gca()
     ax.add_patch(circle)
     plt.scatter(transposedPoints[0], transposedPoints[1], c=color)
