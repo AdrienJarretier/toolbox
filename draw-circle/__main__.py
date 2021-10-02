cd13322e46c67c86201a020964ff2edf596f87e3
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-10-02 10:42:03 +0200

    draw a grid and a circle

--- draw-circle/__main__.py
+++ draw-circle/__main__.py
@@ -1,11 +1,45 @@
 import PySimpleGUI as sg
 
-layout = []
+CANVAS_SIZE = (200, 200)
+BOTTOM_LEFT = (-CANVAS_SIZE[0]/2, -CANVAS_SIZE[1]/2)
+TOP_RIGHT = (CANVAS_SIZE[0]/2, CANVAS_SIZE[1]/2)
+
+graphicalArea = sg.Graph(CANVAS_SIZE, BOTTOM_LEFT, TOP_RIGHT)
+
+layout = [[
+    graphicalArea
+]]
 
 window = sg.Window('Draw Circle', layout, element_padding=(8, 8), font=("default", 14), no_titlebar=False,
                    grab_anywhere=False, use_custom_titlebar=True, titlebar_icon="", enable_close_attempted_event=True)
 
 window.finalize()
+
+
+def drawGrid(cellSize):
+    cellWidth = cellSize[0]
+    cellHeight = cellSize[1]
+    lines = int((TOP_RIGHT[1]-BOTTOM_LEFT[1])/cellHeight)
+    cols = int((TOP_RIGHT[0]-BOTTOM_LEFT[0])/cellWidth)
+
+    for i in range(2):
+        y = i*cellHeight+BOTTOM_LEFT[1]
+        print('y :', y)
+        graphicalArea.draw_line((BOTTOM_LEFT[0], y), (TOP_RIGHT[0], y))
+
+    for j in range(2):
+        x = j*cellWidth+BOTTOM_LEFT[0]
+        print('x :', x)
+        graphicalArea.draw_line((x, TOP_RIGHT[1]), (x, BOTTOM_LEFT[1]))
+
+
+drawGrid((20, 20))
+
+
+circleId = graphicalArea.draw_circle(
+    (0, 0), 50, line_color='black', line_width=4)
+
+print('circle id :', circleId)
 
 # Display and interact with the Window using an Event Loop
 while True:
