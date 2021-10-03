dcd863a436a4884095080b3e2e420f37c469115c
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-10-03 05:37:01 +0200

    fixed grid to be disaplyed correctly with ani grid cell size and any canvas size

--- draw-circle/__main__.py
+++ draw-circle/__main__.py
@@ -1,8 +1,19 @@
 import PySimpleGUI as sg
+import random
 
-CANVAS_SIZE = (200, 200)
+SEED = random.randrange(2**64)
+# SEED = 10487079130662621353
+print('seed :', SEED)
+random.seed(SEED)
+
+CANVAS_SIZE = (random.randint(200, 500), random.randint(200, 500))
+# CANVAS_SIZE = (200, 200)
 BOTTOM_LEFT = (-CANVAS_SIZE[0]/2, -CANVAS_SIZE[1]/2)
 TOP_RIGHT = (CANVAS_SIZE[0]/2, CANVAS_SIZE[1]/2)
+
+print('CANVAS_SIZE :', CANVAS_SIZE)
+print('BOTTOM_LEFT :', BOTTOM_LEFT)
+print('TOP_RIGHT :', TOP_RIGHT)
 
 graphicalArea = sg.Graph(CANVAS_SIZE, BOTTOM_LEFT, TOP_RIGHT)
 
@@ -19,27 +30,37 @@ window.finalize()
 def drawGrid(cellSize):
     cellWidth = cellSize[0]
     cellHeight = cellSize[1]
-    lines = int((TOP_RIGHT[1]-BOTTOM_LEFT[1])/cellHeight)+1
-    cols = int((TOP_RIGHT[0]-BOTTOM_LEFT[0])/cellWidth)+1
+    lines = int(CANVAS_SIZE[1]/cellHeight)+1
+    cols = int(CANVAS_SIZE[0]/cellWidth)+1
 
-    graphicalArea.draw_line(
-        (BOTTOM_LEFT[0], BOTTOM_LEFT[1]+1),
-        (TOP_RIGHT[0], BOTTOM_LEFT[1]+1))
-    for i in range(1, lines):
+    # graphicalArea.draw_line(
+    #     (BOTTOM_LEFT[0], BOTTOM_LEFT[1]+1),
+    #     (TOP_RIGHT[0], BOTTOM_LEFT[1]+1))
+
+    y = BOTTOM_LEFT[1]+1
+    print('y :', y)
+    graphicalArea.draw_line((BOTTOM_LEFT[0], y), (TOP_RIGHT[0], y))
+    for i in range(1, lines+1):
         y = i*cellHeight+BOTTOM_LEFT[1]
         print('y :', y)
         graphicalArea.draw_line((BOTTOM_LEFT[0], y), (TOP_RIGHT[0], y))
 
     for j in range(cols-1):
         x = j*cellWidth+BOTTOM_LEFT[0]
-        print('x :', x)
+        # print('x :', x)
         graphicalArea.draw_line((x, TOP_RIGHT[1]), (x, BOTTOM_LEFT[1]))
+    x = (cols-1)*cellWidth+BOTTOM_LEFT[0]-1
+    # print('x :', x)
     graphicalArea.draw_line(
-        (TOP_RIGHT[0]-1, TOP_RIGHT[1]),
-        (TOP_RIGHT[0]-1, BOTTOM_LEFT[1]))
+        (x, TOP_RIGHT[1]),
+        (x, BOTTOM_LEFT[1]))
 
 
-drawGrid((20, 20))
+# CELLS_SIZE = (CANVAS_SIZE[0]/random.randint(1, 20), CANVAS_SIZE[1]/random.randint(1, 20))
+CELLS_SIZE = (random.randint(10, 100), random.randint(10, 100))
+# CELLS_SIZE = (50, 50)
+print('CELLS_SIZE :', CELLS_SIZE)
+drawGrid(CELLS_SIZE)
 
 
 circleId = graphicalArea.draw_circle(
