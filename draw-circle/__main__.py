df0b644bf9d976d8ed0ebfbedfe06de8de0abfc7
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-10-05 15:05:54 +0200

    erase button, on click erases canvas and redraw grid

--- draw-circle/__main__.py
+++ draw-circle/__main__.py
@@ -9,7 +9,10 @@ gridResolutionInput = sg.Input(
     1, (3, None), key='-GRID_RESOLUTION-')
 ###################### -------------- ######################
 
-layout = [[graphicalArea, sg.Text('grid resolution :'), gridResolutionInput]]
+layout = [[sg.Column([[graphicalArea]]),
+           sg.Column([[sg.Text('grid resolution :'), gridResolutionInput],
+                      [sg.Button('Erase')]])
+           ]]
 
 window = sg.Window('Draw Circle', layout, element_padding=(8, 8), font=("default", 14), no_titlebar=False,
                    grab_anywhere=False, use_custom_titlebar=True, titlebar_icon="", enable_close_attempted_event=True,
@@ -33,6 +36,22 @@ while True:
     # print(values)
     # print('event :', event)
 
+    ###################### get inputs values ######################
+
+    try:
+        gridResolution = int(values['-GRID_RESOLUTION-'])
+    except ValueError:
+        gridResolution = 0
+
+    ###################### ----------------- ######################
+
+    if window.find_element_with_focus() == gridResolutionInput:
+        if event == 'Up:38':
+            gridResolution += 1
+        if event == 'Down:40' and gridResolution > 1:
+            gridResolution -= 1
+        gridResolutionInput.update(gridResolution)
+
     if event == '-GRAPH_AREA-':
         print('drawing circle at', values['-GRAPH_AREA-'])
         graphicalArea.draw_circle(
@@ -44,7 +63,7 @@ while True:
         # tk_canvas
         polygonCenter = graphicalArea.mapCoordsToPixels(
             graphicalArea.snapToGrid(
-            graphicalArea.mapPixelToCoords(values['-GRAPH_AREA-']))
+                graphicalArea.mapPixelToCoords(values['-GRAPH_AREA-']))
         )
         crossSize = 20
         crossPoints = [
@@ -66,21 +85,9 @@ while True:
                 graphicalArea.mapPixelToCoords(values['-GRAPH_AREA-'])),
             2, line_color='black', line_width=1)
 
-    ###################### get inputs values ######################
-
-    try:
-        gridResolution = int(values['-GRID_RESOLUTION-'])
-    except ValueError:
-        gridResolution = 0
-
-    ###################### ----------------- ######################
-
-    if window.find_element_with_focus() == gridResolutionInput:
-        if event == 'Up:38':
-            gridResolution += 1
-        if event == 'Down:40' and gridResolution > 1:
-            gridResolution -= 1
-        gridResolutionInput.update(gridResolution)
+    if event == 'Erase':
+        graphicalArea.erase()
+        graphicalArea.drawGrid()
 
     # print(gridResolution)
 
