ab9cecf047edca7af9d888651db7e4bc9ed05a96
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-10-21 17:19:31 +0200

    take measurements every second

--- shelly-data-collector/__main__.py
+++ shelly-data-collector/__main__.py
@@ -1,6 +1,11 @@
+from os import path
 import urllib.request
 import json
 import pprint
+import time
+import csv
+
+RECORD_INTERVAL = 'SEC'  # MIN / SEC
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -9,9 +14,31 @@ with open('localConfig.json') as configFile:
 
 pp.pprint(config['ips'])
 
-for ip in config['ips']:
-    rawContent = urllib.request.urlopen('http://'+ip+'/meter/0').read()
+with open(path.join('data', 'data.csv'), 'w', newline='') as dataOutputFile:
+    csvWriter = csv.writer(dataOutputFile, delimiter=',')
+    csvWriter.writerow(['timestamp']+config['ips'])
+    while True:
+        timeBefore = time.time()
 
-    meter = json.loads(rawContent)
+        powers = []
+        for ip in config['ips']:
+            rawContent = urllib.request.urlopen('http://'+ip+'/meter/0').read()
 
-    pp.pprint(meter)
+            meter = json.loads(rawContent)
+
+            powers.append({
+                'ip': ip,
+                'power': meter['power'],
+                'timestamp': meter['timestamp']
+            })
+
+        pp.pprint(powers)
+
+        timeEnd = time.time()
+        elapsedTime = timeEnd-timeBefore
+        if RECORD_INTERVAL == 'MIN':
+            time.sleep(60-(meter['timestamp'] % 60)+1)
+        elif RECORD_INTERVAL == 'SEC':
+            time.sleep(max(0, 1-elapsedTime))
+        else:
+            raise 'wrong RECORD_INTERVAL value'
