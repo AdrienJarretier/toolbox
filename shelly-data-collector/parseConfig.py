bc7db54fd02b937f7e35abd72642904a7c10e7e3
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-10-25 16:49:06 +0200

    parseConfig will load templaceLocalConfig and prompt users if localConfig.json is not found

--- shelly-data-collector/parseConfig.py
+++ shelly-data-collector/parseConfig.py
@@ -6,6 +6,7 @@ CONFIG_FILENAME = 'localConfig.json'
 def askForList(endString):
     list = []
     e = ''
+    print('(one per line, end with '+endString+') :')
     while e != endString:
         e = input()
         list.append(e)
@@ -13,6 +14,35 @@ def askForList(endString):
     list.pop()
 
     return list
+
+
+def askForInt():
+    return int(eval(input()))
+
+
+def askForDict(dic, depth=0):
+
+    for k in dic:
+
+        v = dic[k]
+
+        print('  '*depth + k + ' ', end='')
+        if isinstance(v, dict):
+            print()
+            dic[k] = askForDict(v, depth+1)
+        elif isinstance(v, list):
+            dic[k] = askForList('0')
+        elif isinstance(v, int):
+            dic[k] = askForInt()
+
+        # print(k)
+
+    return dic
+
+
+with open('templateLocalConfig.json') as configTemplate:
+    # print(json.load(configTemplate))
+    print(askForDict(json.load(configTemplate)))
 
 
 def loadConfig():
