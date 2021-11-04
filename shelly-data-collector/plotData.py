f6e4bb9b4f756a5758577d0d2f7fb4d03458f28f
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-04 18:12:49 +0100

    when a data point is missing average using points before adn after

--- shelly-data-collector/plotData.py
+++ shelly-data-collector/plotData.py
@@ -15,13 +15,23 @@ x = []
 y = []
 y2 = []
 
+# # returns a list of size n averaged data from originalData
+# def averageToNPoints(originalData, n):
 
-def toFloat(val):
+
+
+def toFloat(lines, i, j):
+    val = lines[i][j]
     try:
         if val != '':
             return float(val)
         else:
-            return 0.0
+            if i == 1:
+                return toFloat(lines, i+1, j)
+            elif i == len(lines)-1:
+                return toFloat(lines, i-1, j)
+            else:
+                return (toFloat(lines, i-1, j)+toFloat(lines, i+1, j))/2
     except ValueError as ve:
         print(ve)
         print(val)
@@ -35,15 +45,16 @@ for f in p.iterdir():
             print(f)
             lines = list(csvReader)
             header = lines[0]
-            for row in lines[1:]:
-                x.append(datetime.fromtimestamp(int(row[0])).isoformat())
-                y.append(toFloat(row[1]))
-                y2.append(toFloat(row[2]))
+            for i in range(1, len(lines)):
+                x.append(datetime.fromtimestamp(int(lines[i][0])).isoformat())
+                y.append(toFloat(lines, i, 1))
+                y2.append(toFloat(lines, i, 2))
 
 
 # df = px.data.gapminder().query("country=='Canada'")
 # fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
 
+# fig = go.Figure(layout=go.Layout(width=1920))
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(x=x, y=y,
