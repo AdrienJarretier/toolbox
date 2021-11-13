5127910bc3ce1c52b5b29cb39f3e867ff0f12275
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-13 12:40:04 +0100

    looop throw all dirs in obeservations and all files and run chi-squared test for each

--- diceBiasTest/__main__.py
+++ diceBiasTest/__main__.py
@@ -3,46 +3,58 @@ import pandas as pd
 from contextlib import redirect_stdout
 import pathlib
 
-DIE_SIDES = 20
+for obsContent in pathlib.Path('observations').iterdir():
 
-observed = [0]*DIE_SIDES
+    if obsContent.is_dir():
 
-with open('observations/d20/values.txt') as values:
-    for v in values:
-        observed[int(v)-1] += 1
+        diceDirPath = obsContent
 
+        DIE_SIDES = int(diceDirPath.name[1:])
 
-expected = [sum(observed)/DIE_SIDES] * DIE_SIDES
+        observed = [0]*DIE_SIDES
 
-chisq, p = chisquare(observed, expected)
+        for obsValuesPath in diceDirPath.iterdir():
 
-for _ in range(2):
-    outputFolderPath = pathlib.Path('testsResults', 'dice')
-    try:
-        with open('testsResults/dice/filename.txt', 'w') as resultsFile:
-            with redirect_stdout(resultsFile):
-                print()
+            with open(obsValuesPath) as values:
+                for v in values:
+                    observed[int(v)-1] += 1
 
-                print('********************* d' +
-                      str(DIE_SIDES), '*********************')
+            expected = [sum(observed)/DIE_SIDES] * DIE_SIDES
 
-                print(pd.DataFrame([expected, observed], ['expected', 'observed'], [
-                    i for i in range(1, len(observed)+1)]))
-                print('\n')
+            chisq, p = chisquare(observed, expected)
 
-                print('======= chi-squared test statistic =======')
-                print(chisq)
-                print('\n')
-                print('======= p-value =======')
-                print(p, end='')
-                if p < 0.05:
-                    print(' *', end='')
-                    if p < 0.01:
-                        print('*', end='')
-                        if p < 0.001:
-                            print('*', end='')
-                print('\n')
-    except FileNotFoundError:
-        outputFolderPath.mkdir(parents=True)
-    else:
-        break
+            print('\n')
+
+            for _ in range(2):
+                outputFolderPath = pathlib.Path('testsResults', diceDirPath.name)
+                try:
+                    outputResultsPath = outputFolderPath.joinpath(obsValuesPath.name)
+                    with open(outputResultsPath, 'w') as resultsFile:
+                        with redirect_stdout(resultsFile):
+                            print()
+
+                            print('********************* d' +
+                                  str(DIE_SIDES), '*********************')
+
+                            print(pd.DataFrame([expected, observed], ['expected', 'observed'], [
+                                i for i in range(1, len(observed)+1)]))
+                            print('\n')
+
+                            print('======= chi-squared test statistic =======')
+                            print(chisq)
+                            print('\n')
+                            print('======= p-value =======')
+                            print(p, end='')
+                            if p < 0.05:
+                                print(' *', end='')
+                                if p < 0.01:
+                                    print('*', end='')
+                                    if p < 0.001:
+                                        print('*', end='')
+                            print('\n')
+
+                        print('tests results in : ', outputResultsPath)
+                except FileNotFoundError:
+                    outputFolderPath.mkdir(parents=True)
+                else:
+                    break
